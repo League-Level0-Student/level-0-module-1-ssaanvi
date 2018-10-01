1ca68863c579d1b81934ba31591fc0d24aa570c2
ssaanvi <league@WTS20.attlocal.net>
2018-10-01 16:55:24 -0700

    10/1/18

--- src/ifs_ints_and_loops/Badger.java
+++ src/ifs_ints_and_loops/Badger.java
@@ -1,23 +1,21 @@
 package ifs_ints_and_loops;
 
 public class Badger {
-public static void main(String[] args) {
-	
+	public static void main(String[] args) {
 
+		String b = "Badger ";
+		String m = "Mushroom ";
+		String s = "Snake";
+		for (int i = 0; i < 2; i++) {
 
-String b="Badger ";
-String m= "mushroom ";
-for(int i=0; i<2; i++) {
-
-for(int j=0; j<11; j++) {
-System.out.print(b);
-}
-System.out.println();
-System.out.print(m);
-System.out.print(m);
-
-}
-
-}
+			for (int j = 0; j < 11; j++) {
+				System.out.print(b);
+			}
+			System.out.println();
+			System.out.print(m);
+			System.out.println(m);
+		}
+		
+	}
 
 }
